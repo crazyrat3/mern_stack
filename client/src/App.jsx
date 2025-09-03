21448d3699604064921f598939302ffbddd86793
crazyrat3 <theclockman0304@gmail.com>
2025-09-03 16:51:32 +0530

    changed frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -38,6 +38,7 @@ function App() {
   return (
     <>
       <div>
+        <h1>Hello This is</h1>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
